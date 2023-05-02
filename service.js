7c125440f5f715de7ec99986900f8fed0ec26ed1
DEBENDRA KUMAR MALLICK <115154630+dmunusonu@users.noreply.github.com>
2023-05-02 22:56:51 +0530

    changes made on service part

---
+++ service.js
@@ -0,0 +1,9 @@
+var dropdownItems = document.querySelectorAll('.dropdown-content a');
+
+for (var i = 0; i < dropdownItems.length; i++) {
+  dropdownItems[i].addEventListener('click', function() {
+    // code to handle the click event goes here
+  });
+}
+
+
